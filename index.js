a8b40724244b240e1792d985c417d56278d85105
qwook <henryquoctran@gmail.com>
2016-10-03 16:07:47 -0700

    ReadFileSync and ReadFile GET requests
    Part 1 of assignment

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var app = express()
+var fs = require('fs');
 
 
 app.set('port', (process.env.PORT || 8080))
@@ -14,8 +15,19 @@ app.get('/', function(request, response) {
  including headers and content,                                                                                                                                                     
  which is why you can only call once*/
 
-
 })
+
+// Part 1
+
+app.get('/readFileSync', function(request, response) {
+    response.send(fs.readFileSync('public/index.html', 'utf8'));
+});
+
+app.get('/readFile', function(request, response) {
+    fs.readFile('public/index.html', 'utf8', function(err, body) {
+      response.send(body);
+    })
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at :" + app.get('port'))
